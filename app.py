31559305d9346383b672b40f4885ef2b7432b9bb
brandonng2 <brandonng0904@gmail.com>
2026-06-06 17:39:08 -0700

    create requrinements.txt

--- app.py
+++ app.py
@@ -97,4 +97,4 @@ def segment():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 3000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
